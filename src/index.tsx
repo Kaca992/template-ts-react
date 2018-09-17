5b6a005d2753383c1e7703b3c0c241e1df881c2b
kacavendadino <kacavendadino@gmail.com>
2018-09-17 19:17:43 +0200

    test git rule

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import Main from '@components/main/main';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
