782fd580f53a63df0d8f00a293a63bbd1c56d65b
Dino Kacavenda <dino.kacavenda@acceleratio.net>
2018-10-15 10:06:04 +0200

    fix za build

--- src/common/config/service.config.ts
+++ src/common/config/service.config.ts
@@ -1,4 +1,4 @@
-import { isProduction } from "src/utils";
+import { isProduction } from "../../utils";
 
 export function appendServiceApiEndpoint(url: string): string {
     return isProduction() ? `https://dinotestfunctions.azurewebsites.net/${url}` : `http://localhost:7071/${url}`;
